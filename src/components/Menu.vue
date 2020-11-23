b4864603578226549155a368a483ac79c2e9b679
v1rt24 <v1rt24@mail.ru>
2020-11-23 15:17:15 +0300

    Создание структуры

---
+++ src/components/Menu.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="sidenav app-sidenav open">
+    <router-link
+        v-for="link of links" :key="link.url"
+        tag="li"
+        :to="{name: link.url}"
+        exact-active-class="active"
+    >
+      <a class="waves-effect waves-orange pointer">{{ link.title }}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  data: () => ({
+    links: [
+      {url: 'Home', title: 'Счёт'},
+      {url: 'History', title: 'История'},
+      {url: 'Planning', title: 'Планирование'},
+      {url: 'Record', title: 'Новая запись'},
+      {url: 'Categories', title: 'Категории'},
+    ],
+  }),
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layouts/Main.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="app-main-layout">
+      <nav class="navbar orange lighten-1">
+        <div class="nav-wrapper">
+          <div class="navbar-left">
+            <a href="#">
+              <i class="material-icons black-text">dehaze</i>
+            </a>
+            <span class="black-text">12.12.12</span>
+          </div>
+
+          <ul class="right hide-on-small-and-down">
+            <li>
+              <a
+                  class="dropdown-trigger black-text"
+                  href="#"
+                  data-target="dropdown"
+              >
+                USER NAME
+                <i class="material-icons right">arrow_drop_down</i>
+              </a>
+
+              <ul id='dropdown' class='dropdown-content'>
+                <li>
+                  <a href="#" class="black-text">
+                    <i class="material-icons">account_circle</i>Профиль
+                  </a>
+                </li>
+                <li class="divider" tabindex="-1"></li>
+                <li>
+                  <a href="#" class="black-text">
+                    <i class="material-icons">assignment_return</i>Выйти
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <Menu/>
+
+      <main class="app-content">
+        <div class="app-page">
+
+          <router-view/>
+
+        </div>
+      </main>
+
+      <div class="fixed-action-btn">
+        <a class="btn-floating btn-large blue" href="#">
+          <i class="large material-icons">add</i>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Menu from '@/components/Menu';
+
+export default {
+  name: 'Main',
+  components: {
+    Menu,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
